381bfa131d40cf437abb967b8ddfb43bc89b520c
Sunny Sun <newzealandsunny@gmail.com>
2022-06-13 20:03:31 -0400

    add server

---
+++ Server/app.js
@@ -0,0 +1,16 @@
+// Set database https://cloud.mongodb.com/v2/62a7cf341bdca30dc93ee0da#clusters
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
